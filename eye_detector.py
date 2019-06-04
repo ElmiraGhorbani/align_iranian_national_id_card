1cb35a4c46e706d95d9a4686c5938d148e937a7d
ElmiiiRaa <elmira_7521@yahoo.com>
2019-06-04 14:41:32 +0430

    First commit

--- eye_detector.py
+++ eye_detector.py
@@ -1,3 +1,27 @@
+# MIT License
+#
+# Copyright (c) 2019 https://github.com/ElmiiiRaa/align_iranian_national_id_card
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#author : Elmira Ghorbani
+#email:elmira.ghorbani96@gmail.com
+
 import cv2
 import numpy as np
 
@@ -29,4 +53,5 @@ def get_eyes(img):
         return (eyes_total[0,:], eyes_total[1,:])
     
     cv2.waitKey(0)
+
 
--- information_exteract.py
+++ information_exteract.py
@@ -20,7 +20,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-#autour : Elmira Ghorbani
+#author : Elmira Ghorbani
 #email:elmira.ghorbani96@gmail.com
 
 import cv2
